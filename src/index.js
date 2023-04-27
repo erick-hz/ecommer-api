264efb27fa0cba88098f78779454b36a5f32233f
Erick Hernandez <yerickk8@gmail.com>
2023-04-27 09:22:06 -0600

    Added Node.js Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+dotenv.config();
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Backend server is running!");
 });
